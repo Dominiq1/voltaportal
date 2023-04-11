86c8f57a9cea93d16bb89f08766a33c9304bf035
dominiq1 <dominiqmartinez13@gmail.com>
2023-04-11 15:08:18 -0700

    color fix

--- pages/Inventory.js
+++ pages/Inventory.js
@@ -82,7 +82,7 @@ export default function Inventory() {
 
 
   return (
-    <Box sx={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    <Box sx={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor:'white', color: 'black'}}>
    
 
       <Container>
