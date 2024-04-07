bccb82df041b39da219525a93416eea3bdd6fb32
Dominiq Martinez <dominiqmartinez13@gmail.com>
2024-04-06 20:42:51 -0700

    main

--- gql/mutations/UserMapInfo.js
+++ gql/mutations/UserMapInfo.js
@@ -23,4 +23,29 @@ const GET_USER_MAP_DATA = gql`
   }
 `;
 
-export { GET_USER_MAP_DATA };
+
+const GET_CONSTRUCTION_MAP_DATA = gql`
+  query GetConstructionUserMapInfo($repID: String!) {
+    GetConstructionUserMapInfo(repID: $repID) {
+      color
+      position {
+        lat
+        lng
+      }
+      label {
+        text
+        color
+      }
+      latitude
+      longitude
+      textPub
+      colorPub
+      installer 
+      installDate
+      projectURL
+    }
+  }
+`;
+
+
+export { GET_USER_MAP_DATA, GET_CONSTRUCTION_MAP_DATA };
