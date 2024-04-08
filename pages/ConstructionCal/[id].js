c1508fcd7ef546918b1b6331ec54ca6891862003
Dominiq Martinez <dominiqmartinez13@gmail.com>
2024-04-08 01:01:42 -0700

    black text color

--- pages/ConstructionCal/[id].js
+++ pages/ConstructionCal/[id].js
@@ -184,7 +184,7 @@ const openLinkInNewTab = (url) => {
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
       >
-        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4 }}>
+        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', color:'black', boxShadow: 24, p: 4 }}>
           <Typography id="modal-title" variant="h6" component="h2">
             Event Details
           </Typography>
