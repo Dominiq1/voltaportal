db6b52f8ee180cd0146a6235c9d0c55663364cdc
Dominiq <marti958@cougars.csusm>
2024-06-14 14:14:43 -0700

    dahs time added

--- components/page/Approve.jsx
+++ components/page/Approve.jsx
@@ -7,21 +7,22 @@ const Approve = ({ result, id }) => {
 
 
 
-
-  // Function to format the date in mm-dd-yy format
+// Function to format the date in YYYY-MM-DD format
 function formatDate(date) {
   let day = ('0' + date.getDate()).slice(-2); // Get the day and pad with zero if needed
   let month = ('0' + (date.getMonth() + 1)).slice(-2); // Get the month, add 1 (since months are zero-indexed), and pad with zero if needed
-  let year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
-  return `${month}-${day}-${year}`; // Concatenate in the desired format
+  let year = date.getFullYear(); // Get the full year
+  return `${year}-${month}-${day}`; // Concatenate in the desired format
 }
-
-// Today's date formatted
-const today = formatDate(new Date());
 
 
 
   const submitFLAresponse = async ({ decisionStatus, eventID }) => {
+
+    // Today's date formatted
+    var today = formatDate(new Date()).toString();
+
+   
     const QB_DOMAIN = "voltaic.quickbase.com";
     const API_ENDPOINT = "https://api.quickbase.com/v1/records";
 
@@ -45,13 +46,13 @@ const today = formatDate(new Date());
       requestBody.data = [{
         3: { value: eventID },
         26: { value: "251" },
-        17: { value: today },
+        17: { value: today  },
       }];
     } else {
       requestBody.data = [{
         3: { value: eventID },
         26: { value: "629" },
-        17: { value: today },//Needs to be date
+        17: { value: today  },//Needs to be date
       }];
     }
 
